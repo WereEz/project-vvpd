666e54c6bec77bfd6e0b8aa8968a48abc557eb0d
Nikita Samarin <yormungandsam@yandex.ru>
2022-03-28 15:35:35 +0700

    Реализован поиск в интернете по словам поиск, найти, найди

--- functional.py
+++ functional.py
@@ -4,8 +4,10 @@ def execute_command(commands, command):
     command.lower()
     if command.find("открой") != -1 or command.find("открыть") != -1:
         return fopen(commands, command)
+    if command.find("поиск") != -1 or command.find("найди") != -1 or command.find("найти") != -1:
+        return fsearch(command)
 
-def fopen(commands ,command):
+def fopen(commands, command):
     for i in commands["folders"].items():
         if command.find(i[1]) != -1:
             #открыть папку, путь хранится в i[0]
@@ -16,3 +18,11 @@ def fopen(commands ,command):
                 webbrowser.open_new_tab(j[0])
                 return 1
     return 0
+
+def fsearch(command):
+    search_string = command.repalce("поиск", "", 1)
+    search_string = search_string.repalce("найди", "", 1)
+    search_string = search_string.repalce("найти", "", 1)
+    search_string = search_string.repalce(" ","+")
+    search_string = "https://www.google.com/search?q=" + search_string
+    webbrowser.open_new_tab(search_string)
