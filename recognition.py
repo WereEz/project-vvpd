18a984b8067d2846f8eb448441aabe305e8f9036
Nikita Samarin <yormungandsam@yandex.ru>
2022-04-11 18:57:19 +0700

    Уменьшена задержка перед началом распознавания, добавлен запрет на запуск несколький помошников(Работает только с exe, exe не обновлён

--- recognition.py
+++ recognition.py
@@ -4,10 +4,11 @@ def google_recognize():
     recognizer = speech_recognition.Recognizer()
     microphone = speech_recognition.Microphone()
     with microphone as audio_file:
-        recognizer.adjust_for_ambient_noise(audio_file)
+        recognizer.adjust_for_ambient_noise(audio_file, duration=0.5)
         audio = recognizer.listen(audio_file)
         try:
             result = recognizer.recognize_google(audio, language = "ru")
+
         except:
             return ("Не распознано")
         return result
